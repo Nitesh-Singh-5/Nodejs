f68c4d138852f7e82e5ecd07ad61bab845f03f3c
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-11-06 21:11:04 +0530

    Learning Module Wrapper

---
+++ Module2/index.js
@@ -0,0 +1,12 @@
+// Module Wrapper
+
+(function (exports, require, module, __filename, __dirname) {
+    // Module code actually lives in here 
+});
+
+
+console.log("Hello world");
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(require);
