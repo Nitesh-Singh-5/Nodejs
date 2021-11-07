4e39542b0ae3e9d78eea6bc8dcde9d4fe4187c40
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-11-07 23:21:33 +0530

    Leaning ES6 import statement in Node js

---
+++ Module4/index.js
@@ -0,0 +1,16 @@
+// ES6 Import statement
+
+import path from 'path';   // To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
+
+console.log(path.basename('C:/Users/ns987/OneDriveDesktop/webdev/Node js/index.js'))
+// console.log(path.basename(__filename));
+// console.log(path.basename(__filename), '.js');
+
+// console.log(path.join(__dirname, 'code', 'allcode.js'));
+
+
+// console.log(path.normalize('c:\\temp\\\\foo\\bar'));
+
+// console.log(path.parse(__filename));
+// console.log(path.parse(__filename).base);
+// console.log(path.parse(__filename).name);
